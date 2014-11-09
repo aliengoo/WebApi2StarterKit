d4c9c6432f930bbf6c5aaa9895db73867efb2f43
Glenn <glenn.android@gmail.com>
2014-11-09 14:31:20 +0000

    Removed packages

--- WebApi2Starter/App_Start/StartUpRouting.cs
+++ WebApi2Starter/App_Start/StartUpRouting.cs
@@ -2,8 +2,6 @@
 {
     using System.Web.Http;
 
-    using Owin;
-
     public static class StartUpRouting
     {
          public static void SetRoutes(this HttpConfiguration configuration)
@@ -21,7 +19,6 @@
                  name: "DefaultApiWithAction",
                  routeTemplate: "api/{controller}/{action}/{id}",
                  defaults: new { id = RouteParameter.Optional });
-
          }
     }
 }
